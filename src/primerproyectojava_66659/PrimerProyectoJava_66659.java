610805d19960b5d708941146b895a3ccdf807466
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-27 20:57:05 -0600

    Primer proyecto Java

---
+++ src/primerproyectojava_66659/PrimerProyectoJava_66659.java
@@ -0,0 +1,29 @@
+/*
+ * UNIVERSIDAD AUTÓNOMA DE CAMPECHE
+ * Facultad de Ingeniería
+ * Ingeniería en Sistemas Computacionales
+ */
+package primerproyectojava_66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class PrimerProyectoJava_66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+      // PROGRAMA QUE DECLARE 3 VARIABLES
+      
+      int entero = 45;
+      char letra = 'f';
+      boolean codificar = true;
+      
+      
+    }
+    
+    }
+    
